a07f0622f232c5981a44c74a2bb0119b74b6ebd2
Conrad Kleinespel <conradk@conradk.com>
2021-02-03 21:17:56 +0100

    moves to rust-2018

--- examples/read-password.rs
+++ examples/read-password.rs
@@ -1,7 +1,5 @@
 //! This example demonstrates how to read a password from STDIN.
 
-extern crate rpassword;
-
 fn main() {
     println!("Prompt:");
     let pass = rpassword::read_password().unwrap();
