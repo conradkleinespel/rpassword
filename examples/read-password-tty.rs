9ff168d19d1b0539d67ba77582f11975bec23dd7
Conrad Kleinespel <conradk@conradk.com>
2021-02-03 01:45:34 +0100

    removes useless examples

--- examples/read-password-tty.rs
+++
@@ -1,9 +0,0 @@
-//! This example demonstrates how to read a password from the tty.
-
-extern crate rpassword;
-
-fn main() {
-    println!("Prompt:");
-    let pass = rpassword::read_password_from_tty().unwrap();
-    println!("Password: {}", pass);
-}
