cf4aa020c0b17f054fa4c33ded7cda18cc27228f
Conrad Kleinespel <conradk@conradk.com>
2021-02-06 23:40:25 +0100

    simplify rprompt and rpassword

--- examples/read-password.rs
+++
@@ -1,7 +0,0 @@
-//! This example demonstrates how to read a password from STDIN.
-
-fn main() {
-    println!("Prompt:");
-    let pass = rpassword::read_password().unwrap();
-    println!("Password: {}", pass);
-}
---
+++ src/rutil/print_tty.rs
@@ -0,0 +1,58 @@
+#[cfg(unix)]
+mod unix {
+    use std::io::Write;
+
+    /// Displays a message on the TTY
+    pub fn print_tty(prompt: impl ToString) -> std::io::Result<()> {
+        let mut stream = std::fs::OpenOptions::new().write(true).open("/dev/tty")?;
+        stream
+            .write_all(prompt.to_string().as_str().as_bytes())
+            .and_then(|_| stream.flush())
+    }
+}
+
+#[cfg(windows)]
+mod windows {
+    use std::io::Write;
+    use std::os::windows::io::FromRawHandle;
+    use winapi::um::fileapi::{CreateFileA, OPEN_EXISTING};
+    use winapi::um::handleapi::INVALID_HANDLE_VALUE;
+    use winapi::um::winnt::{FILE_SHARE_READ, FILE_SHARE_WRITE, GENERIC_READ, GENERIC_WRITE};
+
+    /// Displays a message on the TTY
+    pub fn print_tty(prompt: impl ToString) -> std::io::Result<()> {
+        let handle = unsafe {
+            CreateFileA(
+                b"CONOUT$\x00".as_ptr() as *const i8,
+                GENERIC_READ | GENERIC_WRITE,
+                FILE_SHARE_READ | FILE_SHARE_WRITE,
+                std::ptr::null_mut(),
+                OPEN_EXISTING,
+                0,
+                std::ptr::null_mut(),
+            )
+        };
+        if handle == INVALID_HANDLE_VALUE {
+            return Err(std::io::Error::last_os_error());
+        }
+
+        let mut stream = unsafe { std::fs::File::from_raw_handle(handle) };
+
+        stream
+            .write_all(prompt.to_string().as_str().as_bytes())
+            .and_then(|_| stream.flush())
+    }
+}
+
+/// Prints a message to a writer
+pub fn print_writer(stream: &mut impl Write, prompt: impl ToString) -> std::io::Result<()> {
+    stream
+        .write_all(prompt.to_string().as_str().as_bytes())
+        .and_then(|_| stream.flush())
+}
+
+use std::io::Write;
+#[cfg(unix)]
+pub use unix::print_tty;
+#[cfg(windows)]
+pub use windows::print_tty;
