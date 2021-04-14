edaae61d17639ba88318f15d27b623293c339f02
Conrad Kleinespel <conradk@conradk.com>
2021-04-15 00:18:28 +0200

    Remove opening of pull request
    Github does not allow fast forward merge from UI, we need to merge from
    command line to preserve commit times and signatures

--- src/rpassword/all.rs
+++ src/rpassword/all.rs
@@ -1,3 +1,33 @@
+//! This library makes it easy to read passwords in a console application on all platforms, Unix and
+//! Windows alike.
+//!
+//! Here's how you can read a password:
+//! ```no_run
+//! let password = rpassword::read_password().unwrap();
+//! println!("Your password is {}", password);
+//! ```
+//!
+//! You can also prompt for a password:
+//! ```no_run
+//! let password = rpassword::prompt_password("Your password: ").unwrap();
+//! println!("Your password is {}", password);
+//! ```
+//!
+//! Finally, in unit tests, you might want to pass a `Cursor`, which implements `BufRead`. In that
+//! case, you can use `read_password_from_bufread` and `prompt_password_from_bufread`:
+//! ```
+//! use std::io::Cursor;
+//!
+//! let mut mock_input = Cursor::new("my-password\n".as_bytes().to_owned());
+//! let password = rpassword::read_password_from_bufread(&mut mock_input).unwrap();
+//! println!("Your password is {}", password);
+//!
+//! let mut mock_input = Cursor::new("my-password\n".as_bytes().to_owned());
+//! let mut mock_output = Cursor::new(Vec::new());
+//! let password = rpassword::prompt_password_from_bufread(&mut mock_input, &mut mock_output, "Your password: ").unwrap();
+//! println!("Your password is {}", password);
+//! ```
+
 use crate::rutil::fix_new_line::fix_new_line;
 use crate::rutil::print_tty::{print_tty, print_writer};
 use crate::rutil::safe_string::SafeString;
